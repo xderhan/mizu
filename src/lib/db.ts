86e13fc9a1fc5f03e9bddcebae9de708a5c69142
Sederhana Gulo <derhanvx@gmail.com>
2023-07-30 21:16:49 +0700

    Deploy to fly.io

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,15 +5,15 @@ import { Surreal } from 'surrealdb.js'
  *
  * @returns {Surreal} The SurrealDB connection
  */
-const db = new Surreal('http://127.0.0.1:8000/rpc', {
+const db = new Surreal('https://flysdb.fly.dev/rpc', {
     // Set the namespace and database for the connection
     ns: 'mizu',
     db: 'db',
 
     // Set the authentication details for the connection
     auth: {
-        user: 'root',
-        pass: 'root'
+        user: 'adminpsda',
+        pass: 'adminpsda12345777'
     }
 })
 
